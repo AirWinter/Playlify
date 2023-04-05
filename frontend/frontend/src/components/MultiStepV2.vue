dc164d35d985f4badbafe2a6af6072774da90486
François <francois.ezard@gmail.com>
2023-04-05 22:00:27 +0200

    submit loading logic

--- frontend/frontend/src/components/MultiStepV2.vue
+++ frontend/frontend/src/components/MultiStepV2.vue
@@ -24,6 +24,7 @@ export default {
         { label: "Indie", value: "indie" },
         { label: "R&B", value: "r-n-b" },
       ],
+      loading: false,
     };
   },
   methods: {
@@ -38,6 +39,7 @@ export default {
       }).then((value) => (this.genre_options = value.data));
     },
     async handleSubmit(param) {
+      this.loading = true;
       console.log(param.playlistInformation);
       console.log(param.filters);
       const token =
@@ -74,6 +76,8 @@ export default {
           songs_to_add: songs_to_add_array,
         },
       });
+      this.loading = false;
+      this.$router.push("/my-playlists");
     },
   },
 };
@@ -81,95 +85,100 @@ export default {
 
 <template>
   <h1>Create a Playlist</h1>
-  <!-- we'll get rid of the default form actions -->
-  <FormKit type="form" :actions="false">
-    <FormKit
-      type="multi-step"
-      tab-style="progress"
-      #default="{ value }"
-      :value="playlist"
-    >
-      <FormKit type="step" name="playlistInformation" label="Basic Information">
+  <div v-if="loading">Loading...</div>
+  <div v-else>
+    <!-- we'll get rid of the default form actions -->
+    <FormKit type="form" :actions="false">
+      <FormKit
+        type="multi-step"
+        tab-style="progress"
+        #default="{ value }"
+        :value="playlist"
+      >
         <FormKit
-          type="text"
-          name="name"
-          id="name"
-          label="Playlist Name"
-          placeholder="Name"
-          validation="required|name"
-        />
-        <FormKit
-          type="textarea"
-          name="description"
-          id="description"
-          label="Description (optional)"
-          placeholder="Description"
-          :help="
-            value.playlistInformation?.description !== undefined
-              ? `${value.playlistInformation.description.length} / 300`
-              : `0 / 300`
-          "
-          validation="length:0,300"
-          validation-visibility="live"
-          :validation-messages="{
-            length: 'Description cannot be more than 300 characters.',
-          }"
-        />
-        <FormKit
-          type="checkbox"
-          label="Public"
-          help="Do you want your playlist to be public?"
-          name="public"
-        />
-        <template #stepNext="{ handlers, node }">
-          <!-- incrementStep returns a callable function -->
+          type="step"
+          name="playlistInformation"
+          label="Basic Information"
+        >
           <FormKit
-            type="button"
-            @click="
-              handleNextOne(value), handlers.incrementStep(1, node.context)()
-            "
-            label="Next Step"
-            data-next="true"
+            type="text"
+            name="name"
+            id="name"
+            label="Playlist Name"
+            placeholder="Name"
+            validation="required|name"
           />
-        </template>
-      </FormKit>
-
-      <FormKit type="step" name="filters" label="Filters">
-        <FormKit
-          type="select"
-          label="Genre of your playlist"
-          name="genre"
-          :options="genre_options"
-        />
-
-        <FormKit
-          type="month"
-          help="Add songs created after this date"
-          label="Songs Created After"
-          name="created_after_month"
-        />
-        <FormKit
-          type="month"
-          help="Add songs created before this date"
-          label="Songs Created Before"
-          name="created_before_month"
-        />
-        <template #stepPrevious="{ handlers, node }">
-          <!-- incrementStep returns a callable function -->
           <FormKit
-            type="button"
-            @click="
-              handleSubmit(value), handlers.incrementStep(-1, node.context)()
+            type="textarea"
+            name="description"
+            id="description"
+            label="Description (optional)"
+            placeholder="Description"
+            :help="
+              value.playlistInformation?.description !== undefined
+                ? `${value.playlistInformation.description.length} / 300`
+                : `0 / 300`
             "
-            label="Custom Previous"
+            validation="length:0,300"
+            validation-visibility="live"
+            :validation-messages="{
+              length: 'Description cannot be more than 300 characters.',
+            }"
           />
-        </template>
-        <template #stepNext>
-          <FormKit type="submit" />
-        </template>
+          <FormKit
+            type="checkbox"
+            label="Public"
+            help="Do you want your playlist to be public?"
+            name="public"
+          />
+          <template #stepNext="{ handlers, node }">
+            <!-- incrementStep returns a callable function -->
+            <FormKit
+              type="button"
+              @click="
+                handleNextOne(value), handlers.incrementStep(1, node.context)()
+              "
+              label="Next Step"
+              data-next="true"
+            />
+          </template>
+        </FormKit>
+
+        <FormKit type="step" name="filters" label="Filters">
+          <FormKit
+            type="select"
+            label="Genre of your playlist"
+            name="genre"
+            :options="genre_options"
+          />
+
+          <FormKit
+            type="month"
+            help="Add songs created after this date"
+            label="Songs Created After"
+            name="created_after_month"
+          />
+          <FormKit
+            type="month"
+            help="Add songs created before this date"
+            label="Songs Created Before"
+            name="created_before_month"
+          />
+          <template #stepPrevious="{ handlers, node }">
+            <!-- incrementStep returns a callable function -->
+            <FormKit
+              type="button"
+              @click="handlers.incrementStep(-1, node.context)()"
+              label="Custom Previous"
+            />
+          </template>
+          <template #stepNext>
+            <FormKit type="submit" @click="handleSubmit(value)" />
+          </template>
+        </FormKit>
+        <pre wrap>{{ value }}</pre>
       </FormKit>
-      <pre wrap>{{ value }}</pre>
+      <pre wrap>{{ playlist }}</pre>
     </FormKit>
-    <pre wrap>{{ playlist }}</pre>
-  </FormKit>
+  </div>
 </template>
