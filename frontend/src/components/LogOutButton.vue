40bf3ea4077e6c5f5a12347c1dbee76c08cd084e
François <francois.ezard@gmail.com>
2023-07-09 12:11:18 +0200

    fix logout

--- frontend/src/components/LogOutButton.vue
+++ frontend/src/components/LogOutButton.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Log Out Button -->
   <div class="absolute top-3 right-12 max-sm:top-2 max-sm:right-4">
-    <a :href="urlBase + '/logout'"
+    <a :href="urlBase + '/authentication/logout'"
       ><button
         type="button"
         class="btn bg-snow h-10 w-28 max-sm:h-8 max-sm:w-20 max-sm:text-sm font-semibold rounded-full text-black hover:underline"
