b737a3cba7c98d339adeffda2d4113fd204e7674
François <francois.ezard@gmail.com>
2023-07-16 12:19:19 +0200

    Move logic out of controller + unit tests

---
+++ backend/playlist_model.py
@@ -0,0 +1,58 @@
+from utils import chunks
+
+
+def get_playlists(user, sp):
+    my_playlists = []
+    count = 0
+    while True:
+        playlists = sp.user_playlists(user=user['id'], limit=50, offset=count * 50)
+        for playlist in playlists['items']:
+            image = ''
+            if len(playlist['images']) > 0:
+                image = playlist['images'][0]['url']
+            my_playlists.append(
+                {"name": playlist['name'], "description": playlist['description'], "public": playlist['public'],
+                 "imageUrl": image, 'link': playlist['external_urls']['spotify'],
+                 'creator': playlist['owner']['display_name']})
+        count += 1
+        if len(playlists) < 50:
+            break
+    return my_playlists
+
+
+def create_playlist(user, req, sp):
+    name = req['name']
+    display_on_profile = False  # False by default
+    # If public is specified then overwrite
+    if 'display' in req.keys():
+        display_on_profile = req['display']
+
+    description = req['description']
+
+    # If the user provided a description
+    if description is not None and description != "":
+        response_create = sp.user_playlist_create(user=user['id'], name=name, public=display_on_profile,
+                                                  description=description)
+        if response_create['description'] is None or response_create['description'] == "":
+            count = 0
+            playlist_id = response_create['id']
+            # Try at most 10 times to set the playlist description
+            while count <= 10:
+                response_update = sp.playlist_change_details(playlist_id=playlist_id, description=description)
+                count += 1
+                if response_update['description'] is not None and response_update['description'] != "":
+                    break
+    else:
+        # Create empty playlist without a description
+        response_create = sp.user_playlist_create(user=user['id'], name=name, public=display_on_profile)
+
+    playlist_id = response_create['id']
+
+    songs_to_add_string = req['songs_to_add']
+    songs_to_add = songs_to_add_string.split(",")
+    # print(f"Songs to add: {songs_to_add}")
+
+    # Populate Playlist
+    for list_of_songs in chunks(songs_to_add, 100):
+        sp.playlist_add_items(playlist_id, list_of_songs)
+        # print(f"Populated: {response_populate}")
